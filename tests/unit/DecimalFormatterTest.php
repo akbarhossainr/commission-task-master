3699800c45aaee537811c434658793ebe09ba717
Akbar Hossain Robin <akbar.hossain@dh.health>
2022-11-19 14:56:38 +0600

    add unit test for the DecimalFomatter/format

---
+++ tests/unit/DecimalFormatterTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkbarHossain\CommissionTask\Test\Unit;
+
+use AkbarHossain\CommissionTask\Formatter\DecimalFormatter;
+use AkbarHossain\CommissionTask\Test\TestCase;
+
+class DecimalFormatterTest extends TestCase
+{
+    public function dataProviderForDifferentCurrency(): array
+    {
+        return [
+            'default currency' => [
+                [
+                    'transaction' => $this->createTransactionObject()->setAmount(100.8999),
+                    'expected' => '100.90',
+                ],
+            ],
+            'JPY currency' => [
+                [
+                    'transaction' => $this->createTransactionObject()
+                        ->setCurrency('JPY')
+                        ->setAmount(100.8999),
+                    'expected' => '101',
+                ],
+            ],
+            'USD currency' => [
+                [
+                    'transaction' => $this->createTransactionObject()
+                        ->setCurrency('USD')
+                        ->setAmount(100.8999),
+                    'expected' => '100.90',
+                ],
+            ],
+        ];
+    }
+
+    /** @dataProvider  dataProviderForDifferentCurrency */
+    public function testFormatOutputIsCorrect(array $data)
+    {
+        $formatter = new DecimalFormatter(
+            $this->getActualConfig(),
+            $data['transaction']
+        );
+
+        $amount = $data['transaction']->getAmount();
+
+        $this->assertSame($data['expected'], $formatter->format($amount));
+    }
+}
