184776c629bf986475f727716095f7fe745e3e7e
Akbar Hossain Robin <akbar.hossain@dh.health>
2022-11-19 12:35:12 +0600

    write integration test for BusinessClient service class

---
+++ tests/integration/Service/BusinessClientTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkbarHossain\CommissionTask\Test\Service;
+
+use AkbarHossain\CommissionTask\Formatter\DecimalFormatter;
+use AkbarHossain\CommissionTask\Service\BusinessClient;
+use AkbarHossain\CommissionTask\Test\TestCase;
+
+class BusinessClientTest extends TestCase
+{
+    protected $config;
+    protected $transaction;
+
+    public function setUp(): void
+    {
+        $this->config = $this->getActualConfig();
+    }
+
+    public function dataProviderForBusinessClient()
+    {
+        return [
+            'default' => [
+                [
+                    'transaction' => $this->createTransactionObject()
+                        ->setClient('business'),
+                    'expected' => 7.5,
+                ],
+            ],
+            'withdraw commission fee' => [
+                [
+                    'transaction' => $this->createTransactionObject()
+                        ->setClient('business')
+                        ->setOperationType('withdraw'),
+                    'expected' => 7.5,
+                ]
+            ],
+            'deposit commission fee' => [
+                [
+                    'transaction' => $this->createTransactionObject()
+                        ->setClient('business')
+                        ->setOperationType('deposit'),
+                    'expected' => 0.45,
+                ]
+            ]
+        ];
+    }
+
+    /** @dataProvider dataProviderForBusinessClient */
+    public function testCommissionMethodReturnCorrectResult(array $data)
+    {
+        $businessClient = new BusinessClient(
+            $this->config,
+            $data['transaction']
+        );
+
+        $actual = (new DecimalFormatter($this->config, $data['transaction']))
+            ->format($businessClient->commission());
+
+        $this->assertEquals($data['expected'], $actual);
+    }
+}
